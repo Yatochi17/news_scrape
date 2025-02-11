2d900cc1b90ef8a5abd1c733bc26fbd93f62dd23
Yatochi <aqildanial.17@gmail.com>
2025-02-11 18:51:53 +0800

    3nd commit

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Install Chrome
-apt update && apt install -y wget unzip
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
-# Run the app
-gunicorn app:app
